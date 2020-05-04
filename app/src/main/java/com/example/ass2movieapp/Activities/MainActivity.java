66f22855b24c0a66f21c75d3cb416cf56256ed3e
MunseongJeong <jmunseng@gmail.com>
2020-05-05 03:11:29 +1000

    recycler view and  Dependencies setting

--- app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.ass2movieapp;
+package com.example.ass2movieapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.ass2movieapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
