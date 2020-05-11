660cf682ffd6b50ab2c1747fb783818f0d85d893
MunseongJeong <jmunseng@gmail.com>
2020-05-12 04:49:23 +1000

    Create menu bar

--- app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
@@ -1,18 +1,54 @@
 package com.example.ass2movieapp.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
+import com.example.ass2movieapp.Data.MovieRecyclerViewAdapter;
+import com.example.ass2movieapp.Model.Movie;
 import com.example.ass2movieapp.R;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+private RecyclerView recyclerView;
+private MovieRecyclerViewAdapter movieRecyclerViewAdapter;
+private List<Movie> movies;
+private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(this);
         //READ ME; KEEFU
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.new_search) {
+            showInputDialog();
+            // return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/example/ass2movieapp/Util/Constants.java
@@ -0,0 +1,6 @@
+package com.example.ass2movieapp.Util;
+
+public class Constants {
+    public static final String URL_LEFT = "http://www.omdbapi.com/?s=";
+    public static final String URL_RIGHT = "&page=2";
+}
