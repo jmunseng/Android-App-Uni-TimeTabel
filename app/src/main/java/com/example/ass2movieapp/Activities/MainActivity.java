c2f2dc31e555ae0ec8d674967788e8d68b9f8855
MunseongJeong <jmunseng@gmail.com>
2020-05-14 22:18:24 +1000

    movie list

--- app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
+++ app/src/main/java/com/example/ass2movieapp/Activities/MainActivity.java
@@ -28,13 +28,13 @@ private RequestQueue queue;
         queue = Volley.newRequestQueue(this);
         //READ ME; KEEFU
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -51,4 +51,97 @@ private RequestQueue queue;
         return super.onOptionsItemSelected(item);
     }
 
+
+    public void showInputDialog() {
+
+        alertDialogBuilder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_view, null);
+        final EditText newSearchEdt = (EditText) view.findViewById(R.id.searchEdt);
+        Button submitButton = (Button) view.findViewById(R.id.submitButton);
+
+        alertDialogBuilder.setView(view);
+        dialog = alertDialogBuilder.create();
+        dialog.show();
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Prefs prefs = new Prefs(MainActivity.this);
+
+                if (!newSearchEdt.getText().toString().isEmpty()) {
+
+                    String search = newSearchEdt.getText().toString();
+                    prefs.setSearch(search);
+                    movieList.clear();
+
+                    getMovies(search);
+
+                    // movieRecyclerViewAdapter.notifyDataSetChanged();//Very important!!
+                }
+                dialog.dismiss();
+
+
+            }
+        });
+
+
+
+    }
+
+
+
+
+    //Get movies
+    public List<Movie> getMovies(String searchTerm) {
+        movieList.clear();
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
+                Constants.URL_LEFT + searchTerm + Constants.URL_RIGHT + Constants.API_KEY, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                try{
+                    JSONArray moviesArray = response.getJSONArray("Search");
+
+                    for (int i = 0; i < moviesArray.length(); i++) {
+
+                        JSONObject movieObj = moviesArray.getJSONObject(i);
+
+                        Movie movie = new Movie();
+                        movie.setTitle(movieObj.getString("Title"));
+                        movie.setYear("Year Released: " + movieObj.getString("Year"));
+                        movie.setMovieType("Type: " + movieObj.getString("Type"));
+                        movie.setPoster(movieObj.getString("Poster"));
+                        movie.setImdbId(movieObj.getString("imdbID"));
+
+
+                        // Log.d("Movies: ", movie.getTitle());
+                        movieList.add(movie);
+
+
+                    }
+                    /**
+                     * Very important!! Otherwise, we wont see anything being displayed.
+                     */
+                    movieRecyclerViewAdapter.notifyDataSetChanged();//Important!!
+
+
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        queue.add(jsonObjectRequest);
+
+        return movieList;
+
+    }
 }
