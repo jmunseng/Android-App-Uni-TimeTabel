65e003e1541b95f5cc3581f451653df0209776ab
kefu huang <huangke@deakin.edu.au>
2020-05-24 11:11:36 +1000

    sign up Page Create Click event

--- Registeration/app/src/main/java/com/example/registeration/LoginActivity.java
+++ Registeration/app/src/main/java/com/example/registeration/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.registeration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_main);
+        TextView registerButton = findViewById(R.id.registerButton);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(registerIntent);
+
+            }
+        });
     }
 }
