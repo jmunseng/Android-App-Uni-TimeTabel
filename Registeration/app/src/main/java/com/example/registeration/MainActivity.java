8ba4788c6c8db6d54f64f5973faaa33f54c09706
MunSeong Jeong <jmunseng@gmail.com>
2020-05-28 14:26:36 +1000

    Create Notice List sync with database (error) continue working

--- Registeration/app/src/main/java/com/example/registeration/MainActivity.java
+++ Registeration/app/src/main/java/com/example/registeration/MainActivity.java
@@ -4,22 +4,54 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView noticeListView;
+    private NoticeListAdapter adapter;
+    private List<Notice> noticeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        noticeListView = findViewById(R.id.noticeListView);
+        noticeList = new ArrayList<Notice>();
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+//        noticeList.add(new Notice("test notice.", "test contents", "2020/05/29"));
+        adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
+        noticeListView.setAdapter(adapter);
+
         final Button courseButton = findViewById(R.id.courseButton);
         final Button statisticsButton = findViewById(R.id.statisticsButton);
         final Button scheduleButton = findViewById(R.id.scheduleButton);
         final LinearLayout notice = findViewById(R.id.notice);
+
 
         courseButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,5 +101,72 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String> {
+
+
+        String target;
+
+        @Override
+        protected void onPreExecute() {
+            target = "https://deakin.cafe24.com/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            try {
+                URL url = new URL(target);
+                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+                InputStream inputStream = httpsURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while ((temp = bufferedReader.readLine()) !=null){
+                    stringBuilder.append(temp+ "\n");
+
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpsURLConnection.disconnect();
+                System.out.println("=========get notice");
+                return stringBuilder.toString().trim();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        @Override
+        public void onProgressUpdate(Void... value){
+            super.onProgressUpdate();
+            System.out.println("=========onprogress");
+        }
+        @Override
+        public void onPostExecute(String result){
+            try{
+
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count = 0;
+                String noticeContent, noticeName, noticeDate;
+                while (count < jsonArray.length()) {
+
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    Notice notice = new Notice(noticeContent,noticeName,noticeDate);
+                    System.out.println("======" + noticeContent);
+                    noticeList.add(notice);
+                    count++;
+
+
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
